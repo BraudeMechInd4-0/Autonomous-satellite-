d37a2c4f79f2c8f7c330c33775017f20a39662ad
Elad Denenberg <eladden@gmail.com>
2025-08-29 03:23:25 +0300

    loop through sats

--- project_phase2/codes/matlab/testODEJ2_cpp.m
+++ project_phase2/codes/matlab/testODEJ2_cpp.m
@@ -10,14 +10,14 @@ fprintf('loading...');
 %% parameters for J2 and drag
 whichconsts = 84;
 Cd = 2.2;
-mu = 398600.5;
+mu = 398600.4418;
 Re = 6378.137;
-J2 = 0.01082635854;
+J2 = 1.08263e-3;
 
 %% these values are from the CATCH paper
 deltas = [8,16]; %number of sections per orbit
 Ns = 16; %number of points per section
-tmax = 60*60*24*14; %14 days
+tmax = 60*60*24*30; %14 days
 
 path_to_csv = '../c++ codes/results/';
 %% create a vector struct of all satellites
@@ -26,7 +26,8 @@ jsonStr = fileread(satsfilename);
 satellites = jsondecode(jsonStr);
 fprintf("done\n")
 for i = 1:length(satellites)
-    satname = 'sat';%satellites(i).name;
+%i=2;
+    satname = satellites(i).name;
     A = satellites(i).A*1e-6;
     m = satellites(i).m;
     r0 = satellites(i).r0;
@@ -49,7 +50,7 @@ for i = 1:length(satellites)
                 t_start = (segment - 1) * Sec;  % MATLAB is 1-based, so segment-1
                 t_end = min(t_start + Sec, tmax);  % Handle partial segment at end
 
-                segment_points = 0.5*(t_end-t_start)*(-cos((0:(N-1))/(N-1))+1)+t_start;
+                segment_points = 0.5*(t_end-t_start)*(-cos((0:(N-1))/(N-1)*pi)+1)+t_start;
                 if segment == 1
                     % First segment: include all points
                     tspan = [tspan, segment_points];
@@ -63,7 +64,12 @@ for i = 1:length(satellites)
             fprintf("generating baseline...")
 
             options = odeset('RelTol',1e-14,'AbsTol', 1e-15,'MaxStep',Sec/(N*10));
-            [~,rbaseline] = ode89(@(t,x)orbit_eq_J2_drag(t,x,mu,Cd,A,m,Re,J2),tspan,[r0 v0],options);
+            [ttest,rbaseline] = ode89(@(t,x)orbit_eq_J2_drag(t,x,mu,Cd,A,m,Re,J2),tspan,[r0' v0'],options);
+%             optMPCI.AbsTol = 1e-12;
+%             optMPCI.RelTol = 1e-9;
+%             optMPCI.N = 16;
+%             optMPCI.Sec = Sec;
+%             [ttestMPCI,rtestMPCI] = odeMPCI(@(t,x)orbit_eq_J2_drag(t,x,mu,Cd,A,m,Re,J2),tspan,[r0' v0'],optMPCI);
             
             fprintf("done!\n")
             %% load the relevant data from files:
@@ -74,6 +80,7 @@ for i = 1:length(satellites)
             ode113res = readmatrix([path_to_csv,satname,'_ODE113_final_positions_',num2str(delta),'_',num2str(N),'.csv']);
             MPCIres = readmatrix([path_to_csv,satname,'_MPCI_final_positions_',num2str(delta),'_',num2str(N),'.csv']);
             %% compute errors
+            errt = abs(ttest - MPCIres(:,1));
             err4 = rbaseline(:,1:3) - rk4res(:,2:end);
             err8 = rbaseline(:,1:3) - rk8res(:,2:end);
             err45 = rbaseline(:,1:3) - ode45res(:,2:end);
